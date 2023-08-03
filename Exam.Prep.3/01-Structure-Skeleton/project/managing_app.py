8d5832de51519ab3bbff54a1462470b45e49569c
Simeon Zhelinski <simo66@abv.bg>
2023-08-04 01:50:55 +0300

    Third time alone - 137/150

--- Exam.Prep.3/01-Structure-Skeleton/project/managing_app.py
+++ Exam.Prep.3/01-Structure-Skeleton/project/managing_app.py
@@ -5,25 +5,114 @@ from project.user import User
 
 
 class ManagingApp:
+    VALID_VEHICLES = ["PassengerCar", "CargoVan"]
+
     def __init__(self):
         self.users = []
         self.vehicles = []
         self.routes = []
 
     def register_user(self, first_name: str, last_name: str, driving_license_number: str):
-        pass
+        for user in self.users:
+            if user.driving_license_number == driving_license_number:
+                return f"{driving_license_number} has already been registered to our platform."
+
+        current_user = User(first_name, last_name, driving_license_number)
+        self.users.append(current_user)
+        return f"{first_name} {last_name} was successfully registered under DLN-{driving_license_number}"
 
     def upload_vehicle(self, vehicle_type: str, brand: str, model: str, license_plate_number: str):
-        pass
+        if vehicle_type not in self.VALID_VEHICLES:
+            return f"Vehicle type {vehicle_type} is inaccessible."
+        for v in self.vehicles:
+            if v.license_plate_number == license_plate_number:
+                return f"{license_plate_number} belongs to another vehicle."
+
+        current_vehicle = None
+
+        if vehicle_type == "PassengerCar":
+            current_vehicle = PassengerCar(brand, model, license_plate_number)
+        else:
+            current_vehicle = CargoVan(brand, model, license_plate_number)
+
+        self.vehicles.append(current_vehicle)
+        return f"{brand} {model} was successfully uploaded with LPN-{license_plate_number}."
 
     def allow_route(self, start_point: str, end_point: str, length: float):
-        pass
+        for route in self.routes:
+            if route.start_point == start_point and route.end_point == end_point and route.length == length:
+                return f"{start_point}/{end_point} - {length} km had already been added to our platform."
 
-    def make_trip(self, driving_license_number: str, license_plate_number: str, route_id: int,  is_accident_happened: bool):
-        pass
+            if route.start_point == start_point and route.end_point == end_point and route.length < length:
+                return f"{start_point}/{end_point} shorter route had already been added to our platform."
+
+            if route.start_point == start_point and route.end_point == end_point and route.length > length:
+                route.is_locked = True
+
+        current_route = Route(start_point, end_point, length, len(self.routes) + 1)
+        self.routes.append(current_route)
+        return f"{start_point}/{end_point} - {length} km is unlocked and available to use."
+
+    def make_trip(self, driving_license_number: str, license_plate_number: str, route_id: int,
+                  is_accident_happened: bool):
+        current_user = None
+        current_route = None
+        current_vehicle = None
+
+        for user in self.users:
+            if user.driving_license_number == driving_license_number:
+                if user.is_blocked:
+                    return f"User {driving_license_number} is blocked in the platform! This trip is not allowed."
+                current_user = user
+
+        for v in self.vehicles:
+            if v.license_plate_number == license_plate_number:
+                if v.is_damaged:
+                    return f"Vehicle {license_plate_number} is damaged! This trip is not allowed."
+                current_vehicle = v
+
+        for route in self.routes:
+            if route.route_id == route_id:
+                if route.is_locked:
+                    return f"Route {route_id} is locked! This trip is not allowed."
+                current_route = route
+
+        current_vehicle.drive(current_route.length)
+        if is_accident_happened:
+            current_vehicle.change_status()
+            current_user.decrease_rating()
+
+        else:
+            current_user.increase_rating()
+
+        return current_vehicle.__str__()
 
     def repair_vehicles(self, count: int):
-        pass
+        damaged_vehicles = []
+        for v in self.vehicles:
+            if v.is_damaged:
+                damaged_vehicles.append(v)
+
+        damaged_vehicles.sort(key=lambda v: (v.brand, v.model))
+        if count > len(damaged_vehicles):
+            for v in damaged_vehicles:
+                v.change_status()
+                v.recharge()
+            count = len(damaged_vehicles)
+
+        else:
+            for v in range(len(damaged_vehicles), count):
+                damaged_vehicles[v].change_status()
+                damaged_vehicles[v].recharge()
+
+        return f"{count} vehicles were successfully repaired!"
 
     def users_report(self):
-        pass
+
+        self.users.sort(key=lambda u: -u.rating)
+
+        result = ["*** E-Drive-Rent ***"]
+        for user in self.users:
+            result.append(user.__str__())
+
+        return "\n".join(result)
--- Exam.Prep.3/01-Structure-Skeleton/project/vehicles/cargo_van.py
+++ Exam.Prep.3/01-Structure-Skeleton/project/vehicles/cargo_van.py
@@ -2,10 +2,9 @@ from project.vehicles.base_vehicle import BaseVehicle
 
 
 class CargoVan(BaseVehicle):
-    MAX_MILEAGE = 180.00
 
     def __init__(self, brand: str, model: str, license_plate_number: str):
-        super().__init__(brand, model, license_plate_number, self.MAX_MILEAGE)
+        super().__init__(brand, model, license_plate_number, max_mileage=180)
 
     def drive(self, mileage: float):
-        self.battery_level -= round((mileage / self.MAX_MILEAGE) * 100) - 5
+        self.battery_level -= round((mileage / self.max_mileage) * 100) - 5
